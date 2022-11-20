64cf1988176ba7ac2af2f27ead8dec65ccf91874
Daphne <71684146+duckysheep@users.noreply.github.com>
2022-11-21 00:14:18 +0800

    Api check

--- Daphne_SEI40_Project2/src/App.jsx
+++ Daphne_SEI40_Project2/src/App.jsx
@@ -2,28 +2,48 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [movieData, setMovieData] = useState(0);
+  const [apiData, setApiData] = useState({});
+  const item = "great%20white%20shark";
+  const spacedItem = item.replace(/%20/g, " ");
 
   useEffect(() => {
-    const apiUrl = `https://services.runescape.com/m=itemdb_rs/api/catalogue/items.json?category=9&alpha=c&page=1`;
+    //runescape ge api
+    //https://api.weirdgloop.org/#/exchange/getExchangeCurrentPrice
+    const apiUrl = `https://api.weirdgloop.org/exchange/history/rs/latest?name=${item}&lang=en`;
 
-    const makeApiCall = () => {
-      fetch(apiUrl)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log("apiData", data);
-          setMovieData(data);
-        });
+    const makeApiCall = async () => {
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      console.log("data", Object.values(data)[0]);
+      setApiData(Object.values(data)[0]);
+      // console.log("apiData", apiData);
     };
+
     makeApiCall();
   }, []);
+
+  // useEffect(() => {
+  //   const apiUrl = `https://api.weirdgloop.org/exchange/history/rs/latest?name=${item}&lang=en`;
+  //   const makeApiCall = () => {
+  //     fetch(apiUrl)
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log("data", Object.values(data)[0].price);
+  //         setApiData(data);
+  //         console.log("apiData", apiData);
+  //       });
+  //   };
+  //   makeApiCall();
+  // }, []);
 
   return (
     <div className="App">
       <h1>Project 2</h1>
       <div className="temp">
-        <h2>API #1 check</h2>
-        <h2>API #2 check</h2>
+        <h2>API check</h2>
+        <p>
+          Price of <b>{spacedItem}</b> : <b>{apiData.price}gp</b>
+        </p>
       </div>
     </div>
   );
